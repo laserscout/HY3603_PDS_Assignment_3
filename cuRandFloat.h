a85d290aaf89cf05109c005cc6fdc6d3d48eabd4
Frank Blanning <frankgou@ece.auth.gr>
2019-01-10 22:00:46 +0200

    Added initialization function (on CUDA) and the Makefile. (TESTED)

---
+++ cuRandFloat.h
@@ -0,0 +1,36 @@
+/**********************************************************************
+ *
+ * cuRandFloat.h -- Generate an arbitrary amount of random floats
+ *
+ * Stolen from the CUDA Toolkit documentation
+ * https://docs.nvidia.com/cuda/curand
+ * 
+ * Frank Blanning <frankgou@auth.gr>
+ * John Flionis < @auth.gr>
+ *
+ **********************************************************************/
+
+#ifndef CU_RAND_FLOAT_H
+#define CU_RAND_FLOAT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <curand.h>
+
+#define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
+    printf("Error \"%s\" at %s:%d\n", \
+    cudaGetErrorString(cudaGetLastError()), \
+    __FILE__,__LINE__);\
+    return EXIT_FAILURE;}} while(0)
+
+#define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
+    printf("Error \"%s\" at %s:%d\n", \
+    cudaGetErrorString(cudaGetLastError()), \
+    __FILE__,__LINE__);\
+    return EXIT_FAILURE;}} while(0)
+
+int randFloat(float **out, int size);
+
+#endif /* CU_RAND_FLOAT_H */
+
--- main.cu
+++ main.cu
@@ -8,11 +8,27 @@
  **********************************************************************/
 
 #include <stdio.h>
-#include "cuNearestNeighborHelper.h"
+#include <stdlib.h>
+#include <cuda.h>
+//#include "cuNearestNeighborHelper.h"
+#include "cuRandFloat.h"
 
+#define DIM 3
 
-int main () {
+int main (int argc, char *argv[]) {
 
+  float *Q, *C;
+  int NC,NQ;
+
+  if (argc != 3) {
+    printf("Usage: %s arg1 arg2\n  where NC=2^arg1 and NQ=2^arg2\n",
+	   argv[0]);
+    exit(1);
+  }
+
+  NC = 1<<atoi(argv[1]);
+  NQ = 1<<atoi(argv[2]);
+  
   size_t threadsPerBlock;
   size_t numberOfBlocks;
 
@@ -23,6 +39,30 @@ int main () {
   
   threadsPerBlock = 8*props.warpSize;
   numberOfBlocks  = 50*props.multiProcessorCount;
+
+  randFloat(&Q, NQ);
+  randFloat(&C, NC);
+
+  CUDA_CALL(cudaMemPrefetchAsync(Q, NQ, cudaCpuDeviceId));
+  CUDA_CALL(cudaMemPrefetchAsync(C, NC, cudaCpuDeviceId));
+
+  /* Show result */
+  printf("===Q vector===\n");
+  for(int i = 0; i < NQ ; i++){
+    for (int d=0; d<DIM; d++)
+      printf("%1.4f ", Q[i*DIM+d]);
+    printf("\n");
+  }
+  printf("===C vector===\n");
+  for(int i = 0; i < NC ; i++){
+    for (int d=0; d<DIM; d++)
+      printf("%1.4f ", C[i*DIM+d]);
+    printf("\n");
+  }
+
+  /* Cleanup */
+  CUDA_CALL(cudaFree(Q));
+  CUDA_CALL(cudaFree(C));
 
   return 0;
 }
