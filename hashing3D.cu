994726f178e2e2d726f069aa2b1e9d5187cb2676
johnfli <iflionis@auth.gr>
2019-01-11 15:29:57 +0200

    Altered so that the kernel processes one boxIdx per call

--- hashing3D.cu
+++ hashing3D.cu
@@ -14,7 +14,7 @@
 #define DIM 3
 
 __global__
-void hashingKernel(float *v, int N, int d, float ***boxes, int *boxesSizes)
+void hashingKernel(float *v, int N, int d, int boxIdx, float **pointersToBoxPoints, int size_boxPoints)
 {
 	int proccess = threadIdx.x + blockIdx.x * blockDim.x;
 	int stride = blockDim.x * gridDim.x;
@@ -22,7 +22,6 @@ void hashingKernel(float *v, int N, int d, float ***boxes, int *boxesSizes)
 	int d2 = d * d;
 	int d3 = d * d * d;
 
-	int boxIdx;
 	float x, y, z;
 
 	for (int i = process; i < N; i += stride)
@@ -31,21 +30,9 @@ void hashingKernel(float *v, int N, int d, float ***boxes, int *boxesSizes)
 		y = v[i*DIM+1];
 		z = v[i*DIM+2];
 
-		boxIdx = (int)(x*d) + (int)(y*d2) + (int)(z*d3);
-		boxes[boxIdx][boxesSizes[boxIdx]++] = &v[i*DIM];
+		if(boxIdx == (int)(x*d) + (int)(y*d2) + (int)(z*d3))
+			pointersToBoxPoints[size_boxPoints++] = &v[i*DIM];
 	}
-}
-
-
-__global__
-void InitBoxesSizes(int *boxesSizes, int N)
-{
-	int proccess = threadIdx.x + blockIdx.x * blockDim.x;
-	int stride = blockDim.x * gridDim.x;
-	
-	for (int i = process; i < N; i += stride)
-		boxesSizes[i] = 0;
-		
 }
 
 
@@ -53,27 +40,35 @@ void hashing3D(float *v, int N, int d, float ***boxes, int *boxesSizes)
 {
 	int d3 = d * d * d;
 
-	float ***boxes_temp, **ptr;
+	float ***boxes_temp, **ptr, **pointersToBoxPoints;
 	int *boxesSizes_temp;
-	size_t size;
+	size_t size, size_boxPoints, count;
 
 	CUDA_CALL(cudaMallocManged(&boxesSizes_temp, d3 * sizeof(int)));
 
-	CUDA_CALL(InitBoxesSizes<<<numberOfBlocks, threadsPerBlock>>>(boxesSizes_temp, d3));
-
 	size = d3 * sizeof(float **) + N * sizeof(float*);
+	// https://www.geeksforgeeks.org/dynamically-allocate-2d-array-c/
 
 	CUDA_CALL(cudaMallocManged(&boxes_temp, size));
 
-	ptr =(float **)boxes_temp + d3;
-	for(int i=0;i<d3;i++)
-		boxes_temp[i] = ptr + i*N/d3;
-	// https://www.geeksforgeeks.org/dynamically-allocate-2d-array-c/
+	size_boxPoints = N * sizeof(float*);
+
+	CUDA_CALL(cudaMallocManged(&pointersToBoxPoints, size_boxPoints)); 
 
 	// Making sure that InitBoxesSizes is done initializing boxesSizes_temp before proceeding to hashingKernel launch
 	cudaDeviceSynchronize();
 
-	CUDA_CALL(hashingKernel<<<numberOfBlocks, threadsPerBlock>>>(v, N, d, boxes_temp, boxesSizes_temp));
+	count = 0;
+	ptr =(float **)boxes_temp + d3;
+	for(int i=0;i<d3;i++) {
+		size_boxPoints = 0;
+		CUDA_CALL(hashingKernel<<<numberOfBlocks, threadsPerBlock>>>(v, N, d, i, pointersToBoxPoints, size_boxPoints));
+		boxesSizes_temp[i] = size_boxPoints;
+		boxes_temp[i] = ptr + count;
+		count += size_boxPoints;
+		for(int boxPoints = 0; boxPoints < size_boxPoints; boxPoints++)
+			boxes_temp[i][boxPoints] = &pointersToBoxPoints[boxPoints];
+	}
 
 	// Access of boxes array: boxes[d3][N]; actually it's boxes[d3][boxesSizes[d3]] with boxesSizes[d3] having a sum of N points
 	boxes = boxes_temp;
