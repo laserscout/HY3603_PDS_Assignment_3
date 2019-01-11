cf762a1e588d7ef61c021282d09797817a351753
root <laserscout@gmail.com>
2019-01-11 19:50:57 +0000

    on the fly changes so that is compiles. Added testing

--- cuRandFloat.h
+++ cuRandFloat.h
@@ -18,19 +18,23 @@
 #include <cuda.h>
 #include <curand.h>
 
-#define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
-    printf("Error \"%s\" at %s:%d\n", \
-    cudaGetErrorString(cudaGetLastError()), \
-    __FILE__,__LINE__);\
-    return EXIT_FAILURE;}} while(0)
-
-#define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
-    printf("Error \"%s\" at %s:%d\n", \
-    cudaGetErrorString(cudaGetLastError()), \
-    __FILE__,__LINE__);\
-    return EXIT_FAILURE;}} while(0)
 
 int randFloat(float **out, int size);
 
 #endif /* CU_RAND_FLOAT_H */
 
+#ifndef CUDA_CALL
+#define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
+    printf("Error \"%s\" at %s:%d\n", \
+    cudaGetErrorString(cudaGetLastError()), \
+    __FILE__,__LINE__);\
+    return EXIT_FAILURE;}} while(0)
+#endif
+
+#ifndef CURAND_CALL
+#define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
+    printf("Error \"%s\" at %s:%d\n", \
+    cudaGetErrorString(cudaGetLastError()), \
+    __FILE__,__LINE__);\
+    return EXIT_FAILURE;}} while(0)
+#endif
--- main.cu
+++ main.cu
@@ -12,22 +12,26 @@
 #include <cuda.h>
 //#include "cuNearestNeighborHelper.h"
 #include "cuRandFloat.h"
+#include "hashing3D.h"
 
 #define DIM 3
 
 int main (int argc, char *argv[]) {
 
   float *Q, *C;
-  int NC,NQ;
+//   float ***S, ***Q_to_S;
+  int NC,NQ, d;
+//   int *S_Sizes, *Q_to_S_Sizes;
 
-  if (argc != 3) {
-    printf("Usage: %s arg1 arg2\n  where NC=2^arg1 and NQ=2^arg2\n",
+  if (argc != 4) {
+    printf("Usage: %s arg1 arg2 arg3\n  where NC=2^arg1, NQ=2^arg2 and d=2^arg3\n",
 	   argv[0]);
     exit(1);
   }
 
   NC = 1<<atoi(argv[1]);
   NQ = 1<<atoi(argv[2]);
+  d  = 1<<atoi(argv[3]);
   
   size_t threadsPerBlock;
   size_t numberOfBlocks;
@@ -47,13 +51,13 @@ int main (int argc, char *argv[]) {
   CUDA_CALL(cudaMemPrefetchAsync(C, NC, cudaCpuDeviceId));
 
   /* Show result */
-  printf("===Q vector===\n");
+  printf(" ======Q vector====== \n");
   for(int i = 0; i < NQ ; i++){
     for (int d=0; d<DIM; d++)
       printf("%1.4f ", Q[i*DIM+d]);
     printf("\n");
   }
-  printf("===C vector===\n");
+  printf(" ======C vector====== \n");
   for(int i = 0; i < NC ; i++){
     for (int d=0; d<DIM; d++)
       printf("%1.4f ", C[i*DIM+d]);
@@ -64,14 +68,38 @@ int main (int argc, char *argv[]) {
   int *S_Sizes, *Q_to_S_Sizes;
   
   // Hashing C into d*d*d boxes
-  hashing3D(C, NC, d, S, S_Sizes);
+  hashing3D(C, NC, d, S, S_Sizes, numberOfBlocks, threadsPerBlock);
 
   // Hashing Q into d*d*d boxes
-  hashing3D(Q, NQ, d, Q_to_S, Q_to_S_Sizes);
+  hashing3D(Q, NQ, d, Q_to_S, Q_to_S_Sizes, numberOfBlocks, threadsPerBlock);
+    
+  /* Show result */
+  printf("\nd=%d\n\n",d);
+    
+  printf(" ======S vector======\n");
+  for(int boxid=0;boxid<d*d*d;boxid++){
+      for(int i = 0; i < S_Sizes[boxid] ; i++){
+        for (int d=0; d<DIM; d++)
+          printf("%1.4f ", S[boxid][i][d]);
+        printf(" N=%d\n",S_Sizes[boxid]);
+      }
+  }
+  printf(" ===Q_to_S  vector===\n");
+  for(int boxid=0;boxid<d*d*d;boxid++){
+      for(int i = 0; i < Q_to_S_Sizes[boxid] ; i++){
+        for (int d=0; d<DIM; d++)
+          printf("%1.4f ", Q_to_S[boxid][i][d]);
+        printf(" N=%d\n",Q_to_S_Sizes[boxid]);
+      }
+  }
 
   /* Cleanup */
   CUDA_CALL(cudaFree(Q));
   CUDA_CALL(cudaFree(C));
+  CUDA_CALL(cudaFree(Q_to_S));
+  CUDA_CALL(cudaFree(S));
+  CUDA_CALL(cudaFree(Q_to_S_Sizes));
+  CUDA_CALL(cudaFree(S_Sizes));
 
   return 0;
 }
