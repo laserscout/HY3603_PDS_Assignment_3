2c26c3772e4ca716fdab315af101e341e6ff17fc
johnfli <iflionis@auth.gr>
2019-01-11 15:57:13 +0200

    Added #ifndef CUDA_CALL (may be wrong though)

--- hashing3D.h
+++ hashing3D.h
@@ -14,15 +14,15 @@
 #include <stdlib.h>
 #include <cuda.h>
 
+#ifndef CUDA_CALL
 #define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
     printf("Error \"%s\" at %s:%d\n", \
     cudaGetErrorString(cudaGetLastError()), \
     __FILE__,__LINE__);\
     return EXIT_FAILURE;}} while(0)
+#endif
 
-__global__ void hashingKernel(float *v, int N, int d, float ***boxes, int *boxesSizes);
-
-__global__ void InitBoxesSizes(int *boxesSizes, int N);
+__global__ void hashingKernel(float *v, int N, int d, int boxIdx, float **pointersToBoxPoints, int size_boxPoints);
 
 void hashing3D(float *v, int N, int d, float ***boxes, int *boxesSizes);
 
